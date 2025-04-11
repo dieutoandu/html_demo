a7d005f1d1fa0f3b4cc8017dd9ea1603a5aadd30
dieutoandu <dieutoandu@gmail.com>
2025-04-11 21:34:49 +0800

    new_confirm

---
+++ js/bmi.js
@@ -0,0 +1,31 @@
+function getComment(bmi) {
+    if (bmi < 18.5) {
+        comment = "「體重過輕」，需要多運動，均衡飲食，以增加體能，維持健康！"
+        color = 'cyan'
+    }
+
+    else if (bmi < 24) {
+        comment = "恭喜！「健康體重」，要繼續保持！"
+        color = 'green'
+    }
+
+    else if (bmi < 27) {
+        comment = "「體重過重」了，要小心囉，趕快力行「健康體重管理」！"
+        color = 'orange'
+    }
+    else {
+        comment = "啊～「肥胖」，需要立刻力行「健康體重管理」囉！"
+        color = 'red'
+    }
+
+    return [comment, color];
+
+
+}
+
+
+function get_BMI(height, weight) {
+    const bmi = weight / (height / 100) ** 2;
+    return bmi.toFixed(2);
+
+}
